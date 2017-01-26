f4b0555962c739380f210499ea33cd6964424611
Pierre BONDOERFFER <pbondoer@student.42.fr>
2017-01-26 09:47:31 +0100

    fix ftx_put_ubmp

--- client/libftx2/src/image/ftx_put_ubmp_img.c
+++ client/libftx2/src/image/ftx_put_ubmp_img.c
@@ -6,7 +6,7 @@
 /*   By: hmartzol <hmartzol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/27 20:06:11 by hmartzol          #+#    #+#             */
-/*   Updated: 2017/01/02 15:30:28 by hmartzol         ###   ########.fr       */
+/*   Updated: 2017/01/26 01:52:42 by pbondoer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ t_image	*ftx_put_ubmp_img(t_image *out, const t_point pos, const t_ubmp *img,
 	t_point	l;
 	int		t;
 
-	if (out == NULL || img == NULL)
-		return (out);
-	if (pos.x >= out->size.x || pos.x >= out->size.x || pos.x + img->size.x < 0
-			|| pos.y + img->size.y < 0)
+	if (out == NULL || img == NULL || pos.x >= out->size.x || pos.x >=
+			out->size.x || pos.x + img->size.x < 0 || pos.y + img->size.y < 0)
 		return (out);
 	if (mask != NOMASK && (LOCAL_ENDIAN != BIG_ENDIAN))
 		mask = ft_bswap32(mask);
-	l.x = (pos.x + img->size.x >= out->size.x) ? out->size.x : img->size.x;
-	l.y = (pos.y + img->size.y >= out->size.y) ? out->size.y : img->size.y;
+	l.x = (pos.x + img->size.x >= out->size.x) ?
+		MAX(img->size.x, out->size.x) - pos.x : img->size.x;
+	l.y = (pos.y + img->size.y >= out->size.y) ?
+		MAX(img->size.y, out->size.y) - pos.y : img->size.y;
 	posi.y = ((pos.y > 0) ? -1 : -pos.y - 1);
 	while (++posi.y < l.y && !(0 & (posi.x = ((pos.x > 0) ? -1 : -pos.x - 1))))
 		while (++posi.x < l.x)
